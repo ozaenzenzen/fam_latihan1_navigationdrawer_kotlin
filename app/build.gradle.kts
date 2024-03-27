6206ed42e18b3b51ec827eb9090bf0af8fc368c2
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-27 11:21:57 +0700

    update: up sdk version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mynavigationdrawer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mynavigationdrawer"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
